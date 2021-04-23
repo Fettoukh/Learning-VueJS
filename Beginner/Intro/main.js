0b94ac24126f1dc1e0c07bfde85a94739cb879a6
Fettoukh <medamine.fettoukh@gmail.com>
2021-04-23 15:14:14 +0000

    Beginner Intro Vue 2 : List Rendering

--- Beginner/Intro/main.js
+++ Beginner/Intro/main.js
@@ -1,1 +1,21 @@
-var product = "Socks";
+var app = new Vue({
+  el: "#app", //connect to div with id app in our index.js
+  data: {
+    product: "Socks",
+    image: "./images/vmSocks-green-onWhite.jpg",
+    inStock: true,
+    inventory: 100,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+      },
+    ],
+    cart: 0,
+  },
+});
